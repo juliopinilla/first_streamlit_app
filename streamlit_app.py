fe2f4716f88cc6c60e4f1eb04f32cf3d20be6b61
juliopinilla <144628818+juliopinilla@users.noreply.github.com>
2023-09-16 00:21:30 +0200

    Update streamlit_app.py
    adding error control

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,20 @@ streamlit.dataframe(fruits_to_show);
 
 #New section api
 streamlit.header("Fruityvice Fruit Advice!");
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please enter fruit to get information")
+  else
+      streamlit.write('The user entered ', fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# this puts the values in tables 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
-# this show
-streamlit.dataframe(fruityvice_normalized);
+      # this puts the values in tables 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # this show
+      streamlit.dataframe(fruityvice_normalized);
+except URLError as e:
+  streamlit.error();
 
 streamlit.stop()
 #snowflake
