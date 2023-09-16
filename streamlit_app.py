800f40d05fc17f792a461670c6a03059ce068b0e
juliopinilla <144628818+juliopinilla@users.noreply.github.com>
2023-09-16 17:57:23 +0200

    Update streamlit_app.py
    add your own fruti

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,19 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
+
+#snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list;")
+      my_data_rows = my_cur.fetchall()
+      return my_data_rows;
+
+#snowflake insert fruit 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
+      return "Thanks for adding fruit " + new_fruit;
   
 #New section api
 streamlit.header("Fruityvice Fruit Advice!");
@@ -45,22 +58,17 @@ except URLError as e:
   streamlit.error();
 
 
-#snowflake related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-      my_cur.execute("SELECT * from fruit_load_list;")
-      my_data_rows = my_cur.fetchall()
-      return my_data_rows;
-
 # Adding a button to get fruit list
+streamlit.header("View Our Fruit List - Add Your Favorites")
 if streamlit.button("Get Fruit List"):
     streamlit.header("The fruit load contains:")
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_fruit_list = get_fruit_load_list()
     streamlit.dataframe(my_data_fruit_list);
 
-streamlit.stop()
-
 #add fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','tomato')
-streamlit.write('Thanks for adding ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button("Add a Fruit to the list:"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snoflake(add_my_fruit)
+    streamlit.write(back_from_function);
